a17d5e06f78357bd9d34711bbe36a6ab4e0ad751
dfernandezperez <daniel.fernandez.perez@gmail.com>
2025-06-27 16:07:31 +0200

    Update slurm job

--- slurm_execute_snakemake.sh
+++
@@ -1,3 +0,0 @@
-# Usage --> ./execute_pipeline.sh  <rules> (can be nothing or a rule defined in the snakefile without wildcards in the input files)
-snakemake -j 1 --unlock
-snakemake --profile config/snakemake_profile/slurm "$@" > snakemake.log 2> snakemake.err < /dev/null
